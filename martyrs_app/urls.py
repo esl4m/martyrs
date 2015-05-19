3fdf550b0b7cfc084145f7dcd5ac8f753ad3c1e3
islam diaa <islam.diaa@tedata.net>
2015-05-19 13:42:42 +0200

    Adds and delete new cases

--- martyrs_app/urls.py
+++ martyrs_app/urls.py
@@ -5,10 +5,12 @@
 #
 
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.add, name='add'),
+    url(r'^delete/$', views.delete, name='delete'),
+    # url(r'^edit/$', views.edit, name='edit'),
     # url(r'^remove/id/$', views.remove_profile, name='remove'),
 ]
--- martyrs_app/views.py
+++ martyrs_app/views.py
@@ -4,8 +4,9 @@
 #       18 May 2015
 #
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 from .models import MartyrsProfile
 
 
@@ -16,6 +17,44 @@ def index(request):
         'res_list': res_list,
     })
     return HttpResponse(template.render(context))
+
+
+def add(request):
+    new_case = MartyrsProfile(name=request.POST['name'],
+                              date_of_birth=request.POST['date_of_birth'],
+                              date_of_death=request.POST['date_of_death'],
+                              cause_of_death=request.POST['cause_of_death'],
+                              officer_name=request.POST['officer_name'],
+                              governorate=request.POST['governorate'])
+    new_case.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request):
+    case_id = request.POST.get('id')
+    MartyrsProfile.objects.filter(id=case_id).delete()
+    
+    # case = MartyrsProfile.objects.get(id=request.POST.get['id'])
+    # case.delete()
+
+    return HttpResponseRedirect(reverse('index'))
+
+
+# def edit(request):
+#     case = MartyrsProfile.objects.get(id=request.POST['id'])
+#     if request.POST['submitButton'] == 'Update':
+#         case.name = request.POST['name']
+#         case.date_of_birth = request.POST['date_of_birth']
+#         case.date_of_death = request.POST['date_of_death']
+#         case.cause_of_death = request.POST['cause_of_death']
+#         case.officer_name = request.POST['officer_name']
+#         case.governorate = request.POST['governorate']
+#         case.save()
+#
+#     elif request.POST['submitButton'] == 'Delete':
+#         case.delete()
+#
+#     return HttpResponseRedirect(reverse('index'))
 
 #
 # def remove_profile(request):
